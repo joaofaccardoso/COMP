9fbd7d40a852a0663ed784e90cd583b6554de87e
joaofaccardoso <joaofaccardoso@gmail.com>
2020-03-09 12:27:38 +0000

    180 points only I left to do

--- Projeto/Testes/strings.java
+++ Projeto/Testes/strings.java
@@ -1,4 +1,22 @@
+"jnjksdkjnded" 1"cdjhb  dhh a" ."rrrr"2 "mig\uel"
+"cdjhb\g  dhh a" "rrrr"
 
+// "\r  \" \f \n \a  \u \d"
 
-1231
-"ow1a     "
+"	"
+"\r       "
+"\f"
+"\n"
+"\t"
+"\\"
+"\""
+
+"\+"
+"\."
+"\n\n"
+"\<"
+"\h"
+"\p"
+"\"\""
+"\r \f \n \t \\ \""
+"\("
