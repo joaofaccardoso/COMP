8b897faf40baa6e1322fc09c9e7fc9f8b894913e
Leonor <leonor.coelho@gmail.com>
2020-03-28 14:51:21 +0000

    Subproblema D - 55 pontos

--- Projeto/compile.sh
+++ Projeto/compile.sh
@@ -1,6 +1,6 @@
 lex jucompiler.l
 yacc -y -d -t -v jucompiler.y
 cc -o jucompiler y.tab.c lex.yy.c
-./jucompiler -t < Testes/Meta2/statement.java
+./jucompiler -e2 < Testes/Meta2/statement.java
 
 # | diff Testes/unterminatedString.out -
