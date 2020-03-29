1fec83a8be983964ad70b5282f8302c0e202f30d
João Cardoso <joaofaccardoso@gmail.com>
2020-03-29 15:51:37 +0100

    changed var names on functions

--- Projeto/functions.c
+++ Projeto/functions.c
@@ -4,11 +4,11 @@
 #include <string.h>
 #include <stdio.h>
 
-is_vs_decl_list* insert_vs_decl_list(is_vs_decl_list* head, is_vs_decl* declaration) {
+is_vs_decl_list* insert_vs_decl_list(is_vs_decl_list* head, is_vs_decl* ivd) {
     is_vs_decl_list* ivsdl = (is_vs_decl_list*)malloc(sizeof(is_vs_decl_list));
     is_vs_decl_list* tmp;
 
-    ivsdl->decl = declaration;
+    ivsdl->decl = ivd;
     ivsdl->next = NULL;
 
     if (head==NULL) {
@@ -23,12 +23,12 @@ is_vs_decl_list* insert_vs_decl_list(is_vs_decl_list* head, is_vs_decl* declarat
 
 //      STATEMENT INSERT FUNCTION TO BE WRITTEN
 
-is_vs_decl* insert_var_decl(char* var_type, char* var_id) {
+is_vs_decl* insert_var_decl(char* type, char* id) {
     is_vs_decl* ivsd = (is_vs_decl*)malloc(sizeof(is_vs_decl));
     is_var_decl* ivd = (is_var_decl*)malloc(sizeof(is_var_decl));
 
-    ivd->type = var_type;
-    ivd->id = var_id;
+    ivd->type = type;
+    ivd->id = id;
 
     ivsd->wich_decl = d_var;
     ivsd->decl_val.var_decl = ivd;
@@ -36,28 +36,28 @@ is_vs_decl* insert_var_decl(char* var_type, char* var_id) {
     return ivsd;
 }
 
-is_method_body* insert_method_body(is_vs_decl_list* list) {
+is_method_body* insert_method_body(is_vs_decl_list* ivdl) {
     is_method_body* imb = (is_method_body*)malloc(sizeof(is_method_body));
 
-    imb->var_statement_list = list;
+    imb->var_statement_list = ivdl;
 
     return imb;
 }
 
-is_method_param* insert_method_param(char* param_type, char* param_id) {
+is_method_param* insert_method_param(char* type, char* id) {
     is_method_param* imp = (is_method_param*)malloc(sizeof(is_method_param));
 
-    imp->type = param_type;
-    imp->id = param_id;
+    imp->type = type;
+    imp->id = id;
 
     return imp;
 }
 
-is_param_decl_list* insert_method_param_list(is_param_decl_list* head, is_method_param* method_param) {
+is_param_decl_list* insert_method_param_list(is_param_decl_list* head, is_method_param* imp) {
     is_param_decl_list* ipdl = (is_param_decl_list*)malloc(sizeof(is_param_decl_list));
     is_param_decl_list* tmp;
 
-    ipdl->param = method_param;
+    ipdl->param = imp;
     ipdl->next = NULL;
 
     if (head==NULL) {
@@ -70,21 +70,21 @@ is_param_decl_list* insert_method_param_list(is_param_decl_list* head, is_method
     return head;
 }
 
-is_method_header* insert_method_header(char* method_type, char* method_id, is_param_decl_list* method_params) {
+is_method_header* insert_method_header(char* type, char* id, is_param_decl_list* ipdl) {
     is_method_header* imh = (is_method_header*)malloc(sizeof(is_method_header));
 
-    imh->type = method_type;
-    imh->id = method_id;
-    imh->param_list = method_params;
+    imh->type = type;
+    imh->id = id;
+    imh->param_list = ipdl;
 
     return imh;
 }
 
-is_mf_decl_list* insert_mf_decl_list(is_mf_decl_list* head, is_mf_decl* declaration) {
+is_mf_decl_list* insert_mf_decl_list(is_mf_decl_list* head, is_mf_decl* imfd) {
     is_mf_decl_list* imfdl = (is_mf_decl_list*)malloc(sizeof(is_mf_decl_list));
     is_mf_decl_list* tmp;
 
-    imfdl->method_field_decl = declaration;
+    imfdl->method_field_decl = imfd;
     imfdl->next=NULL;
 
     if(head==NULL) {
@@ -97,12 +97,12 @@ is_mf_decl_list* insert_mf_decl_list(is_mf_decl_list* head, is_mf_decl* declarat
     return head;
 }
 
-is_mf_decl* insert_method(is_method_header* header, is_method_body* body) {
+is_mf_decl* insert_method(is_method_header* imh, is_method_body* imb) {
     is_mf_decl* imfd = (is_mf_decl*)malloc(sizeof(is_mf_decl));
     is_method_decl* imd = (is_method_decl*)malloc(sizeof(is_method_decl));
 
-    imd->method_header = header;
-    imd->method_body = body;
+    imd->method_header = imh;
+    imd->method_body = imb;
 
     imfd->wich_decl = d_method;
     imfd->decl_val.method_decl = imd;
@@ -110,12 +110,12 @@ is_mf_decl* insert_method(is_method_header* header, is_method_body* body) {
     return imfd;
 }
 
-is_mf_decl* insert_field(char* field_type, char* field_id) {
+is_mf_decl* insert_field(char* type, char* id) {
     is_mf_decl* imfd = (is_mf_decl*)malloc(sizeof(is_mf_decl));
     is_field_decl* ifd = (is_field_decl*)malloc(sizeof(is_field_decl));
 
-    ifd->type = field_type;
-    ifd->id = field_id;
+    ifd->type = type;
+    ifd->id = id;
 
     imfd->wich_decl = d_field;
     imfd->decl_val.field_decl = ifd;
@@ -123,11 +123,11 @@ is_mf_decl* insert_field(char* field_type, char* field_id) {
     return imfd;
 }
 
-is_program* insert_program(char* program_id, is_mf_decl_list* program_method_field_list) {
+is_program* insert_program(char* id, is_mf_decl_list* imfdl) {
     is_program* ip = (is_program*)malloc(sizeof(is_program));
 
-    ip->id = program_id;
-    ip->method_field_list = program_method_field_list;
+    ip->id = id;
+    ip->method_field_list = imfdl;
 
     return ip;
 }
