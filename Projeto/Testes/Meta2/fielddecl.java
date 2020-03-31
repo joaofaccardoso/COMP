8c605a2de8e2ea534bd9e4e816232701335aba69
João <joaofaccardoso@gmail.com>
2020-03-31 14:00:45 +0100

    print done

--- Projeto/Testes/Meta2/fielddecl.java
+++ Projeto/Testes/Meta2/fielddecl.java
@@ -2,6 +2,8 @@ class Factorial {
     public static boolean b, a, c;
     public static boolean a(){};
     public static int factorial(int n) {
-        Integer.parseInt(args[0]);    
+        Integer.parseInt(args[0]);
+        System.out.print("I'm a string!");
+        System.out.print(2+1);
     }
 }
